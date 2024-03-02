dab700a8a0d6a548d565a5e5c74f524da2ff8d91
William Vita <williamvita@hotmail.com>
2024-03-02 11:22:57 -0400

    doc_2

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView # agregada para administrar usuarios desde la API
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView,  # agregada para administrar usuarios desde la API
 
 
 
